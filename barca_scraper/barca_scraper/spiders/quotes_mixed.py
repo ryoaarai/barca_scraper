5a176179a24a48499ba9c0ae0d0da9123cbc40b3
ryoaarai <ryoaarai@amazon.co.jp>
2025-04-27 23:11:03 +0900

    Add files via upload

---
+++ barca_scraper/barca_scraper/spiders/quotes_mixed.py
@@ -0,0 +1,14 @@
+import scrapy
+
+class MixedSpider(scrapy.Spider):
+    name = "mixed_spider"
+    allowed_domains = ["quotes.toscrape.com"]
+    start_urls = ["https://quotes.toscrape.com/"]
+
+    def parse(self, response):
+        for quote in response.css("div.quote"):
+            yield {
+                "text": quote.css("span.text::text").get(),
+                "author": quote.css("small.author::text").get(),
+                "tags": quote.css("div.tags a.tag::text").getall(),
+            }
